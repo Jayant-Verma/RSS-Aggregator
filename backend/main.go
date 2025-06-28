479c8551185bb2ce8e3f8adac158bf17b4840e5d
Jayant Verma <jayantverma8533@gmail.com>
2025-06-28 11:31:03 +0530

    updated code for ipv4 db connection

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
 
-	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 
 	"github.com/Jayant-Verma/rssagg/internal/database"
 	"github.com/go-chi/chi"
@@ -21,7 +23,10 @@ type apiConfig struct {
 }
 
 func main() {
-	godotenv.Load("../.env")
+	// Load local .env only in development
+	if os.Getenv("RENDER") != "true" {
+		godotenv.Load("../.env")
+	}
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
@@ -33,10 +38,27 @@ func main() {
 		log.Fatal("DB_URL environment variable not set")
 	}
 
-	conn, err := sql.Open("pgx", dbURL)
+	// Parse pgx config
+	config, err := pgx.ParseConfig(dbURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to parse DB_URL: %v", err)
 	}
+
+	// Force IPv4 DNS resolution
+	dialer := &net.Dialer{
+		Timeout:   5 * time.Second,
+		KeepAlive: 5 * time.Second,
+		Resolver: &net.Resolver{
+			PreferGo: true,
+			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+				return net.Dial("tcp4", address)
+			},
+		},
+	}
+
+	config.DialFunc = dialer.DialContext
+
+	conn := stdlib.OpenDB(*config)
 
 	db := database.New(conn)
 	apiCfg := apiConfig{
@@ -78,8 +100,8 @@ func main() {
 	}
 
 	log.Printf("Server listening on port %s", portString)
-	error := srv.ListenAndServe()
-	if error != nil {
+	err = srv.ListenAndServe()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
